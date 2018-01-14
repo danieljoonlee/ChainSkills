614d397f64503e32e61d81fd9512c55e7ba096e2
Daniel J Lee <danieljoonlee@gmail.com>
2018-01-14 13:52:28 -0800

    initial

---
+++ Training/chainlist/truffle.js
@@ -0,0 +1,21 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
+};
+
+// module.exports = {
+//   // See <http://truffleframework.com/docs/advanced/configuration>
+//   // for more about customizing your Truffle configuration!
+//   networks: {
+//     development: {
+//       host: "127.0.0.1",
+//       port: 7545,
+//       network_id: "*" // Match any network id
+//     }
+//   }
+// };
